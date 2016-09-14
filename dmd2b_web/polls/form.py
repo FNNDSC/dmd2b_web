3a8041cbc8be9f3722cea5b692ef93718599d33a
Yves <yves.verpillieux@gmail.com>
2016-09-14 15:55:49 -0400

    Update

--- dmd2b_web/polls/form.py
+++ dmd2b_web/polls/form.py
@@ -3,20 +3,25 @@ from django import forms
 from polls.views import *
 from polls.models import *
 
+
+
 class PatientForm(forms.ModelForm):
     class Meta:
         model = PatientDetails
         fields = ('PatientID', 'PatientSex', 'PatientBirthDate', 'Age_Days', 'PatientName', 'PatientReportedAge')
+
 
 class StudyForm(forms.ModelForm):
     class Meta:
         model = StudyDetails
         fields = ('patient', 'StudyID', 'StudyDescription', 'StudyDate')
 
+
 class SeriesForm(forms.ModelForm):
     class Meta:
         model = SeriesDetails
         fields = ('patient', 'study', 'SeriesID', 'SeriesDescription', 'Modality')
+
 
 class HeaderForm(forms.ModelForm):
     class Meta:
