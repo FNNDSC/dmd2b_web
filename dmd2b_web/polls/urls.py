a284ec3ef387d8a4e9c27a104d3d81feb477584f
YV38 <yves.verpillieux@gmail.com>
2016-11-01 13:31:16 +0100

    Update urls.py

--- dmd2b_web/polls/urls.py
+++ dmd2b_web/polls/urls.py
@@ -8,15 +8,13 @@ app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.PatientList.as_view(), name='patient'),
     
-    url(r'^patient-search/$', views.PatientSearchView.as_view(), name='patient-search'),
-    
-    url(r'^patient-standalone-search/$', views.PatientStandaloneSearchView.as_view(), name='patient-standalone-search'),
-
     url(r'^(?P<pk>[0-1]+)/$', views.StudyList.as_view(), name='study'),
 
     url(r'^(?P<pk>[1-2]+)/$', views.SeriesList.as_view(), name='serie'),
 
     url(r'^(?P<pk>[2-3]+)/$', views.HeaderList.as_view(), name='header'),
+    
+    url(r'^patient-search/$', views.PatientSearchView.as_view(), name='patient-search'),
 
     url(r'^patient/new/$', views.PatientFormView.as_view(), name='form'),
 
@@ -25,4 +23,6 @@ urlpatterns = [
     url(r'^serie/new/$', views.SeriesFormView.as_view(), name='form'),
 
     url(r'^header/new/$', views.HeaderFormView.as_view(), name='form'),
+    
+    url(r'^patient-standalone-search/$', views.PatientStandaloneSearchView.as_view(), name='patient-standalone-search'),
 ]
